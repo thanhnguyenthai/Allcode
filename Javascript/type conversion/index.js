407881467ca75370f6f0fbc1dc709a7eeaa28242
WINKEY <80002036+winkeyt@users.noreply.github.com>
2022-12-02 15:53:14 +0000

    Add files via upload

---
+++ Javascript/type conversion/index.js
@@ -0,0 +1,25 @@
+// type conversion = change the datatype of a value to another 
+//              (strings, numbers, booleans)
+/*
+let age = window.prompt("How old r u?: ");
+
+console.log(typeof age);
+age = Number(age);10
+console.log(typeof age);
+age+=1;
+
+console.log("HPBD : ", age, "Years old");
+
+*/
+
+
+let x;
+let y;
+let z;
+
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("Pizza");
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
